21212dde8ae9ab4510210f5ae7192b12350b0e7f
Anastasia-ITIAM <anastasiia_degtiar@icloud.com>
2025-06-22 20:51:57 +0200

    Initial commit

---
+++ libs/listing.php
@@ -0,0 +1,12 @@
+<?php
+
+function getListings(): array
+{
+   return [
+    
+    ["title" => "Rocket League PS4", "price" => 20, "image" => "rocket-league.jpg"],
+    ["title" => "Test2", "price" => 90, "image" => "rocket-league.jpg"],
+    ["title" => "Test3", "price" => 80, "image" => "rocket-league.jpg"],
+
+   ];
+}
