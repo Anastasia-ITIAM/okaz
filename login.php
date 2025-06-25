fe0e09647c941e961ae6db079a3aa08b7539e24d
Anastasia-ITIAM <anastasiia_degtiar@icloud.com>
2025-06-25 10:25:03 +0200

    chanhement

--- login.php
+++ login.php
@@ -1,24 +1,49 @@
 <?php
-require_once 'templates/header.php';
+session_start();
+require_once "libs/pdo.php";
+require_once "libs/user.php";
+require_once "templates/header.php";
+
+$error = null;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $user = verifyUserLoginPassword($pdo, $_POST["email"], $_POST["password"]);
+    if ($user) {
+        session_regenerate_id(true);
+        $_SESSION["user"] = [
+            "id" => $user["id"],
+            "username" => $user["username"]
+        ];
+        header("Location: index.php");
+    } else {
+        $error = "Email ou mot de passe incorrect";
+    }
+}
+
 ?>
-<div class=form-signin w-100>
-    <form method="post"> 
-        <h1 class="h3 mb-3 fw-normal">Se connecter</h1>
-        <div class="form-floating mb-3"> 
-        <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com"> 
-        <label for="floatingInput">Email</label> 
+
+<div class="form-signin w-100 m-auto">
+    <h1>Se connecter</h1>
+    <form method="post">
+
+        <div class="form-floating mb-2">
+            <input name="email" type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email</label>
         </div>
-        <div class="form-floating mb-3"> 
-        <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password"> 
-        <label for="floatingPassword">Mot de passe</label> 
+        <div class="form-floating  mb-2">
+            <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Mot de passe</label>
         </div>
+        <?php if ($error): ?>
+            <div class="alert alert-danger" role="alert">
+                <?= $error ?>
+            </div>
+        <?php endif; ?>
         <button class="btn btn-primary w-100 py-2" type="submit">Connexion</button>
+
     </form>
 </div>
 
-
-
-
 <?php
-require_once 'templates/footer.php';
+require_once "templates/footer.php";
 ?>
--- templates/header.php
+++ templates/header.php
@@ -1,13 +1,16 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="assets/css/override-bootstrap.css">
-    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
     <title>Okaz</title>
 </head>
 
@@ -19,13 +22,21 @@
                     <img width="120" src="assets/images/logo-okaz.png" alt="Logo Okaz">
                 </a>
             </div>
+
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                 <li><a href="index.php" class="nav-link px-2 link-secondary">Accueil</a></li>
                 <li><a href="annonces.php" class="nav-link px-2">Annonces</a></li>
             </ul>
+
             <div class="col-md-3 text-end">
-            <a href="login.php" class="btn btn-outline-primary me-2">Connexion</a>
-            <a href="inscription.php" class="btn btn-primary">Inscription</a>
+                <?php if (isset($_SESSION["user"])): ?>
+                    <span>Bonjour <?= $_SESSION["user"]["username"] ?></span>
+
+                    <a class="btn btn-primary" href="logout.php">Déconnexion</a>
+                <?php else: ?>
+                    <a class="btn btn-outline-primary me-2" href="login.php">Connexion</a>
+                    <a class="btn btn-primary" href="inscription.php">Inscription</a>
+                <?php endif; ?>
             </div>
         </header>
         <main>
