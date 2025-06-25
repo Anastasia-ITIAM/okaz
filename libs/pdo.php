cbe6909a2c14c922574a8fb52e34256d72e0ebe2
Anastasia-ITIAM <anastasiia_degtiar@icloud.com>
2025-06-25 12:38:11 +0200

    pdo

--- libs/pdo.php
+++ libs/pdo.php
@@ -1,8 +1,15 @@
 <?php
-
 try {
     $config = parse_ini_file($_SERVER["DOCUMENT_ROOT"] . "/.env");
-    $pdo = new PDO("mysql:dbname={$config["db_name"]};host={$config["db_host"]};charset=utf8mb4", $config["db_user"], $config["db_password"]);
+
+    $dsn = "mysql:dbname={$config['db_name']};host={$config['db_host']};charset=utf8mb4";
+    if (!empty($config['db_port'])) {
+        $dsn .= ";port={$config['db_port']}";
+    }
+
+    $pdo = new PDO($dsn, $config['db_user'], $config['db_password']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (Exception $e) {
-    die("Erreur: {$e->getMessage()}");
+    die("Erreur: " . $e->getMessage());
 }
